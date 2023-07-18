642164c30e1b78ab2dde3e81404ea85c09d5f811
Mantuska95 <kudirkamantas.yahoo.lt>
2023-07-18 20:07:05 +0300

    First commit.

---
+++ src/main/java/lt/codeAcademy/AukcionoAPI/AukcionoApiApplication.java
@@ -0,0 +1,13 @@
+package lt.codeAcademy.AukcionoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AukcionoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AukcionoApiApplication.class, args);
+	}
+
+}
